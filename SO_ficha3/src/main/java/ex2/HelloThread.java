15ee01ec21537b6737f29ae5802b06ca88e86048
MiguelBarbosa04 <75489472+MiguelBarbosa04@users.noreply.github.com>
2023-10-17 10:46:57 +0100

    +

---
+++ SO_ficha3/src/main/java/ex2/HelloThread.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+/**
+ *
+ * @author migue
+ */
+public class HelloThread implements Runnable {
+
+    private int threadNumber;
+
+    public HelloThread(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println("[" + threadName + "] Eu sou uma thread!");
+    }
+
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Modo de uso: java Ex1 <comando>");
+            System.exit(0);
+        }
+
+        int number = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < number; i++) {
+            Thread thread = new Thread(new HelloThread(i));
+            thread.setName("Thread " + i);
+            thread.start();
+        }
+
+    }
+}
