a66905214cbe6b6717af7b10fc924a691ad5a33e
MiguelBarbosa04 <miguelbarbosa2002@outlook.pt>
2023-11-21 21:43:10 +0000

    +

---
+++ SD_SocketsUDPJava/src/main/java/ex1Parte2/ChatClient.java
@@ -0,0 +1,59 @@
+package ex1Parte2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 8080;
+    private static String clientName;
+
+    public static void main(String[] args) {
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            System.out.println("Connected to ChatServer on " + SERVER_ADDRESS + ":" + SERVER_PORT);
+
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String serverResponse;
+                    while ((serverResponse = in.readLine()) != null) {
+                        System.out.println(serverResponse);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveThread.start();
+
+            Scanner scanner = new Scanner(System.in);
+
+            Thread sendThread = new Thread(() -> {
+                System.out.print("Enter your name: ");
+                clientName = scanner.nextLine();
+                out.println(clientName);
+
+                String userInput;
+                while (true) {
+                    System.out.print("Enter message: ");
+                    userInput = scanner.nextLine();
+                    out.println(userInput);
+                }
+            });
+            sendThread.start();
+
+            // Wait for both threads to finish
+            receiveThread.join();
+            sendThread.join();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SD_SocketsUDPJava/src/main/java/ex1Parte2/MultiClientChatServer.java
@@ -0,0 +1,93 @@
+package ex1Parte2;
+
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class MultiClientChatServer {
+    private static final int PORT = 8080;
+    private static List<ClientHandler> clients = new ArrayList<>();
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("ChatServer is running on port " + PORT);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress());
+                ClientHandler client = new ClientHandler(clientSocket);
+                clients.add(client);
+                client.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static class ClientHandler extends Thread {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String clientName;
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void run() {
+            try {
+                out.println("Enter your name:");
+                clientName = in.readLine();
+                out.println("Welcome to the chat, " + clientName + "!");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/private")) {
+                        sendPrivateMessage(message);
+                    } else {
+                        broadcastMessage("[" + getTimestamp() + "] " + clientName + ": " + message);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                clients.remove(this);
+                broadcastMessage("[" + getTimestamp() + "] " + clientName + " has left the chat.");
+            }
+        }
+        private void sendPrivateMessage(String message) {
+            String[] parts = message.split(" ", 3);
+            if (parts.length == 3) {
+                String recipient = parts[1];
+                String privateMessage = "[" + getTimestamp() + "] " + clientName + " (Private): " + parts[2];
+                for (ClientHandler client : clients) {
+                    if (client.clientName.equals(recipient)) {
+                        client.out.println(privateMessage);
+                        return;
+                    }
+                }
+                out.println("User '" + recipient + "' not found.");
+            } else {
+                out.println("Invalid private message format. Use '/private <recipient> <message>'.");
+            }
+        }
+        private void broadcastMessage(String message) {
+            for (ClientHandler client : clients) {
+                if (client != this) {
+                    client.out.println(message);
+                }
+            }
+        }
+        private String getTimestamp() {
+            return new SimpleDateFormat("HH:mm:ss").format(new Date());
+        }
+    }
+}
