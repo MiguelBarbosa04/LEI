094812b00c6cb38673491e1efb8ab2e951258239
MiguelBarbosa04 <miguelbarbosa2002@outlook.pt>
2023-11-28 21:43:17 +0000

    +

---
+++ TesteModelo1/main.c
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Miguel
+ *
+ * Created on 28 de novembro de 2023, 20:23
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+int obtemValor() {
+
+    int r1;
+    do {
+        printf("Diga o valor de 0 a 2:");
+        scanf("%d", &r1);
+    } while (r1 != 1 && r1 != 0 && r1 != 2);
+
+    return r1;
+
+}
+
+void imprimeMatriz(int matriz[3][3]) {
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+int verificaPosicaoVazia(int matriz[3][3], int linha, int coluna) {
+
+    if (matriz[linha][coluna] == -1) {
+        printf("Disponivel");
+        return 1;
+    } else {
+        printf("Não Disponivel");
+        return 0;
+    }
+
+
+}
+
+int verificaVitoria(int matriz[3][3], int numJogador) {
+
+    int i = 0;
+    int j = 0;
+
+    //Verifica diagonais 1
+    for (int i = 0; i < 3; i++) {
+        if (matriz[i][j] == numJogador) {
+            int count = 0;
+            count++;
+            j++;
+            if (count == 3) {
+                printf("O jogador %d GANHOUUUUUUU", numJogador);
+            }
+        }
+    }
+
+
+
+    //Verifica diagonais 21
+    for (int i = 3; i > -1; i--) {
+        if (matriz[i][j] == numJogador) {
+            int count = 0;
+            count++;
+            j++;
+            if (count == 3) {
+                printf("O jogador %d GANHOUUUUUUU", numJogador);
+            }
+        }
+    }
+
+    //Verifica linha
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matriz[i][j] == numJogador) {
+                int count;
+                count++;
+                if (count == 3) {
+                    printf("O jogador %d GANHOUUUUUUU", numJogador);
+                }
+            }
+        }
+    }
+
+    //Verifica coluna
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            if (matriz[i][j] == numJogador) {
+                int count;
+                count++;
+                if (count == 3) {
+                    printf("O jogador %d GANHOUUUUUUU", numJogador);
+                }
+            }
+        }
+    }
+
+
+
+}
+
+int main(int argc, char** argv) {
+
+    int linhas = 3, colunas = 3;
+    int matriz[linhas][colunas];
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            matriz[i][j] = -1;
+        }
+        printf("\n");
+    }
+
+
+    imprimeMatriz(matriz);
+    int linhaJogada = obtemValor();
+    int colunaJogada = obtemValor();
+    verificaPosicaoVazia(matriz, linhaJogada, colunaJogada);
+    verificaVitoria(matriz, 1);
+
+    return (EXIT_SUCCESS);
+}
+
